fddde4a18b056a8d65ac2d7e89e7565682a0d93d
Juan David <juandavid918@gmail.com>
2024-04-14 15:47:04 -0500

    Pros Cons Discusser finalizado tanto backend como frontend

--- react-gpt/src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ react-gpt/src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox } from "../../components";
+import { ProsConsUseCase } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
 export const ProsConsPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async(text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text, isGpt: false }]);
+
+    const { ok, content } = await ProsConsUseCase(text);
+
+    if(!ok) return;
+    
+    setMessages((prev) => [...prev, {
+      text: content,
+      isGpt: true
+    }]);
+
+    setIsLoading(false);
+  }
+
   return (
-    <div>ProsConsPage</div>
-  )
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessage text="Puedes escribir lo que sea que quieres que compare y te de mis puntos de vista." />
+
+          { messages.map((message, index) =>  (
+            message.isGpt
+              ? <GptMessage key={ index } text={ message.text } />
+              : <MyMessage key={ index } text={ message.text } />
+          )) }
+
+          { isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          ) }
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={ handlePost }
+        placeholder="Escribe aquí lo que deseas"
+        disableCorrections
+      />
+    </div>
+  );
 }
