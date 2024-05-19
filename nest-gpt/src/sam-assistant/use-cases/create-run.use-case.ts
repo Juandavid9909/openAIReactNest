5044fd8e9616ddd05c3bad8ed8983bfca4a53fac
Juan David <juandavid918@gmail.com>
2024-05-19 13:11:27 -0500

    Backend de asistente finalizado

---
+++ nest-gpt/src/sam-assistant/use-cases/create-run.use-case.ts
@@ -0,0 +1,17 @@
+import OpenAI from "openai";
+
+interface Options {
+    threadId: string;
+    assistantId?: string;
+}
+
+export const createRunUseCase = async(openai: OpenAI, options: Options) => {
+    const { threadId, assistantId = process.env.OPENAI_ASSISTANT_ID } = options;
+
+    const run = await openai.beta.threads.runs.create(threadId, {
+        assistant_id: assistantId
+        // instructions; // Ojo! Sobreescribe el asistente
+    });
+
+    return run;
+}
